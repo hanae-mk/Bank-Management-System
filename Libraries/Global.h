4d3e01f9bfcc005f305e2887afbb20c6c8119c8a
hanae-mk <hanaae.messari@gmail.com>
2026-01-03 16:25:21 +0100

    Clean project structure and fix include paths

---
+++ Libraries/Global.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include "clsUser.h"
+#include "clsLoginScreen.h"
+
+
+const std::string DATA_PATH = "../Data/";
+
+clsUser CurrentUser = clsUser::Find("", ""); //Dummy Find
+
+
